a5dc04c56c089a611a37f1ef6d75423e29137b2c
stacia <razifrizqullah@Gmail.com>
2020-07-11 17:04:16 -0700

    new

---
+++ myTwitterAPI - shared.py
@@ -0,0 +1,15 @@
+def consumer_key():
+    consumer_key = 'secret'
+    return consumer_key
+
+def consumer_secret():
+    consumer_secret = 'secret'
+    return consumer_secret
+
+def access_token():
+    access_token = 'secret'
+    return access_token
+
+def access_token_secret():
+    access_token_secret = 'secret'
+    return access_token_secret
